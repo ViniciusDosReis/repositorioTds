72ecbf3c69f935c163c8024351226d32a3fa3969
Vinicius <Vinicius@gmail.com>
2024-06-27 15:35:39 -0300

    Adicionando Mensagens se o cadastro foi concluido ou não.

--- LeiloesTDSat/src/cadastroVIEW.java
+++ LeiloesTDSat/src/cadastroVIEW.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -140,6 +143,8 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
+        
+        try {
         ProdutosDTO produto = new ProdutosDTO();
         String nome = cadastroNome.getText();
         String valor = cadastroValor.getText();
@@ -147,9 +152,19 @@ public class cadastroVIEW extends javax.swing.JFrame {
         produto.setNome(nome);
         produto.setValor(Integer.parseInt(valor));
         produto.setStatus(status);
-        
+
         ProdutosDAO produtodao = new ProdutosDAO();
         produtodao.cadastrarProduto(produto);
+
+        // Exibir mensagem de sucesso
+        JOptionPane.showMessageDialog(this, "Produto cadastrado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+    } catch (NumberFormatException e) {
+        // Exibir mensagem de erro para valor inválido
+        JOptionPane.showMessageDialog(this, "Valor inválido! Por favor, insira um número válido.", "Erro", JOptionPane.ERROR_MESSAGE);
+    } catch (Exception e) {
+        // Exibir mensagem de erro geral
+        JOptionPane.showMessageDialog(this, "Erro ao cadastrar produto. Por favor, tente novamente.", "Erro", JOptionPane.ERROR_MESSAGE);
+    }
         
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
