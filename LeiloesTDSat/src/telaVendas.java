67507d935b57db57d155aebb0e0cb5ebe367046d
Vinicius <Vinicius@gmail.com>
2024-06-29 21:01:04 -0300

    Implementando navegação do botão Consultar vendas

--- LeiloesTDSat/src/telaVendas.java
+++ LeiloesTDSat/src/telaVendas.java
@@ -109,7 +109,8 @@ public class telaVendas extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        listagemVIEW listagem = new listagemVIEW(); 
+        listagem.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
